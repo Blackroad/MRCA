a1f01e3351ab447b000fe7c461ac52f3fc7a55c8
Blackroad <VladVell@gmail.com>
2016-12-21 18:06:07 +0200

    Card Reader checker

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,6 +19,7 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
+
         self.session = SessionHelper(self)
         self.unit = UnitHelper(self)
         self.config = config
